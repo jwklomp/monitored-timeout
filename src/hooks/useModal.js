5bab0397a7891ada372281a4804f41562e3b765d
jklomp <jan-willem.klomp@ebpi.nl>
2019-06-19 13:11:05 +0200

    Added comment

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+/**
+ * Hook for showing and hiding a modal component and set it's content
+ * @returns {object} value of isShowing, show function and content from the Hook, so the component can access them
+ */
 const useModal = () => {
   const [isShowing, setIsShowing] = useState(false);
   const [content, setContent] = useState();
